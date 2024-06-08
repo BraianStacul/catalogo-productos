29a8d30281d5faa4ed04a9aaa53f7139f1906e17
BraianStacul <braianstacul000@gmail.com>
2024-06-08 15:44:17 -0300

    Se configuró la carpeta static en settings, se corrigieron los templates, se agregó la funcionalidad para inciar sesión y listar usuarios

--- catalogo_productos/apps/usuarios/views.py
+++ catalogo_productos/apps/usuarios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from apps.usuarios.models import Usuario
+
+def listar_usuarios(request):
+    template_name = 'usuarios/listar_todos.html'
+    lista_usuarios = Usuario.objects.all().order_by("id")
+
+    ctx = {
+        'usuarios': lista_usuarios,
+    }
+    
+    return render(request, template_name, ctx)
--- catalogo_productos/catalogo_productos/urls.py
+++ catalogo_productos/catalogo_productos/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as views_django
 from django.urls import path, include
 
 from . import views
@@ -8,7 +9,8 @@ urlpatterns = [
     path('', views.home, name='pagina_principal'),
 
     # Login & Logout
-
+    path('login/', views.login, name="iniciar_sesion"),
+    
     # Includes
-    path("usuarios/", include('apps.usuarios.url'))
+    path("usuarios/", include('apps.usuarios.urls'))
 ]
