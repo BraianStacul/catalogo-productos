193f119eb47f1fd5dbc09436e4b3955fb640fe17
BraianStacul <braianstacul000@gmail.com>
2024-06-16 22:56:24 -0300

    Se arreglo el diseño del filterset para buscar por nombre de producto

--- catalogo_productos/apps/productos/filterset.py
+++ catalogo_productos/apps/productos/filterset.py
@@ -1,11 +1,19 @@
 import django_filters
-
+from django import forms
 from .models import Producto
 
 class ProductoFilter(django_filters.FilterSet):
 
-    nombre = django_filters.CharFilter(lookup_expr='icontains')
+    buscador = django_filters.CharFilter(
+        field_name='nombre',
+        lookup_expr='icontains',
+        label='Nombre',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ingrese el nombre de la materia para buscar'
+        })
+    )
 
     class Meta:
         model = Producto
-        fields =  ["nombre"]
+        fields = ['buscador']
